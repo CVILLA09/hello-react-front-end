3101e7be0bdaea8bcc003d77b68388a75b168186
Cristian Villa <c.villavirgen@gmail.com>
2023-12-13 01:30:30 -0600

    Restructure the app to follow redux principles and functional ity

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Greeting from './Greeting';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Greetings from './components/Greetings';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-          <Link to="/greeting" className="App-link">Go to Greeting</Link>
-        </header>
-
-        <Route path="/greeting" component={Greeting} />
-      </div>
-    </Router>
-  );
-}
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Greetings />} />
+    </Routes>
+  </BrowserRouter>
+);
 
 export default App;
