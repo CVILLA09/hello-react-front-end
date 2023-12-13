86cc347eb3b3bcc53839406369043248caf6a398
Cristian Villa <c.villavirgen@gmail.com>
2023-12-13 00:32:32 -0600

    Import BrowserRouter and Route from react-router-dom and set up routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
+// Dummy Greeting component for demonstration
+function Greeting() {
+  return <h2>Greeting Page</h2>;
+}
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Edit <code>src/App.js</code> and save to reload.</p>
+          {/* Link to Greeting component */}
+          <Link to="/greeting" className="App-link">Go to Greeting</Link>
+        </header>
+
+        {/* Route Definitions */}
+        <Route path="/greeting" component={Greeting} />
+      </div>
+    </Router>
   );
 }
 
