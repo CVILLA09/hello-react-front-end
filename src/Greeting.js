9807e72865acf65c959a8ce568ec259f35c04037
Cristian Villa <c.villavirgen@gmail.com>
2023-12-13 00:55:49 -0600

    Connect the Greeting Component to the Redux Store

--- src/Greeting.js
+++ src/Greeting.js
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchGreetingRequest, fetchGreetingSuccess, fetchGreetingFailure } from './actions/greetingActions';
 
-function Greeting() {
+function Greeting({ greetingData, dispatch }) {
+  useEffect(() => {
+    // Here I will make the API call to my Rails backend
+    // For now, let's just dispatch a success action with a static message
+    dispatch(fetchGreetingRequest());
+    dispatch(fetchGreetingSuccess('Hello from Redux!'));
+  }, [dispatch]);
+
   return (
     <div>
       <h2>Greeting Page</h2>
-      <p>Welcome to the Greeting Page!</p>
+      {greetingData.loading && <p>Loading...</p>}
+      {greetingData.greeting && <p>{greetingData.greeting}</p>}
+      {greetingData.error && <p>Error: {greetingData.error}</p>}
     </div>
   );
 }
 
-export default Greeting;
+const mapStateToProps = state => {
+  return {
+    greetingData: state.greeting
+  };
+};
+
+export default connect(mapStateToProps)(Greeting);
